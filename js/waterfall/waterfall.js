f1a7f0b9d48db4ef72e665ea0521d755e74ea93b
flamecho <saikaiew@gmail.com>
2026-05-31 21:56:00 +0800

    Site updated: 2026-05-31 21:55:58

--- js/waterfall/waterfall.js
+++ js/waterfall/waterfall.js
@@ -35,32 +35,17 @@ function waterfall(a) {
 
   "string" == typeof a && (a = document.querySelector(a));
 
-  // 获取所有子元素
   var l = [].map.call(a.children, function (a) {
     return a;
   });
 
   a.style.position = "relative";
 
-  // 记录每个元素的原始位置和目标位置
-  var currentPositions = [];
-  var targetPositions = [];
-
-  // 记录当前实际位置
-  l.forEach(function(item) {
-    var rect = item.getBoundingClientRect();
-    var parentRect = a.getBoundingClientRect();
-    currentPositions.push({
-      top: rect.top - parentRect.top,
-      left: rect.left - parentRect.left
-    });
-  });
-
-  // 重置所有元素的位置样式，准备重新计算
+  // 重置所有元素的位置样式
   l.forEach(function(item) {
     item.style.position = "absolute";
-    item.style.top = "auto";
-    item.style.left = "auto";
+    item.style.top = "";
+    item.style.left = "";
     item.style.transition = "";
   });
 
@@ -86,37 +71,6 @@ function waterfall(a) {
   var s = m[0];
   a.style.height = c(h(s) + b("Bottom", s));
   var t = f(a);
-
-  // 获取计算后的目标位置
-  l.forEach(function(item, index) {
-    var rect = item.getBoundingClientRect();
-    var parentRect = a.getBoundingClientRect();
-    targetPositions[index] = {
-      top: rect.top - parentRect.top,
-      left: rect.left - parentRect.left
-    };
-  });
-
-  // 添加过渡动画
-  l.forEach(function(item, index) {
-    var current = currentPositions[index];
-    var target = targetPositions[index];
-
-    if (current && target) {
-      // 先设置到当前位置
-      item.style.transition = "none";
-      item.style.top = c(current.top);
-      item.style.left = c(current.left);
-
-      // 强制重绘
-      item.offsetHeight;
-
-      // 设置过渡动画，移动到目标位置
-      item.style.transition = "top 0.4s cubic-bezier(0.2, 0.9, 0.4, 1.1), left 0.4s cubic-bezier(0.2, 0.9, 0.4, 1.1)";
-      item.style.top = c(target.top);
-      item.style.left = c(target.left);
-    }
-  });
 
   window.addEventListener ? window.addEventListener("resize", k) : (document.body.onresize = k);
 }
